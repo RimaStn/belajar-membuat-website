bde9abb6af0eae7646bae2cac9f5cf6ee1e39a0a
Rima siti nurrohmah <rimasitinurrohmah33@gmail.com>
2021-03-27 11:25:00 +0700

    web

---
+++ assets/style/style.js
@@ -0,0 +1,48 @@
+
+
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+
+function mixtapeFunction() {
+    document.getElementById("listMixtape").style.display = "block";
+}
+
+
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var x = document.getElementById("listMixtape"); 
+
+function closeDialog() { 
+  x.close(); 
+} 
